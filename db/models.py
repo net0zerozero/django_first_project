b1631a56d6d89096a39cf818e8dc8e328e52bc1a
Net_Zero <netrannetranner@gmail.com>
2023-02-22 22:04:08 +0600

    Add new models to project
    Added the following models to the project:
    - ContentRatings
    - Customers
    - Genres
    - Platforms
    - Publishers
    - Products
    - discount
    - Sales
    - WishList
    - WL_P
    
    Также обновлен базовый класс «Модель», чтобы включить поля «id», «created_at» и «updated_at». Кроме того, создан новый базовый класс `Model2` с `creat$
    
    Эти модели были добавлены для поддержки функционала проекта и будут использоваться для хранения данных, связанных с рейтингами контента, клиентами, жанрами, платформами, $
    
    Note: This commit message was generated by ChatGPT, a language model trained by OpenAI.

--- db/models.py
+++ db/models.py
@@ -11,6 +11,76 @@ class Model(models.Model):
     class Meta:
         abstract = True
 
-class Product(Model):
+
+
+class Model2(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True, db_index=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+
+
+class ContentRatings(Model):
     title = models.CharField(max_length=255)
+    ageLimit = models.IntegerField()
+    description = models.TextField()
+
+
+class Customers(Model):
+    firstName = models.CharField(max_length=30)
+    lastName = models.CharField(max_length=30)
+    email = models.CharField(max_length=50)
+    phoneNum = models.CharField(max_length=20)
+    dateOfBirth = models.DateField()
+
+
+class Genres(Model):
+    title = models.CharField(max_length=255)
+
+
+class Platforms(Model):
+    title = models.CharField(max_length=255)
+
+
+class Publishers(Model):
+    title = models.CharField(max_length=255)
+
+
+class Products(Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    platform = models.ForeignKey(Platforms, on_delete=models.CASCADE)
+    publisher = models.ForeignKey(Publishers, on_delete=models.CASCADE)
+    releaseDate = models.DateField()
     price = models.IntegerField()
+    contentRating = models.ForeignKey(ContentRatings, on_delete=models.CASCADE)
+    genre = models.ForeignKey(Genres, on_delete=models.CASCADE)
+    isAvailable = models.BooleanField()
+
+
+class discount(Model):
+    product = models.ForeignKey(Products, on_delete=models.CASCADE)
+    dateFrom = models.DateField()
+    dateUntill = models.DateField()
+    price = models.IntegerField()
+
+
+class Sales(Model):
+    product = models.ForeignKey(Products, on_delete=models.CASCADE)
+    customer = models.ForeignKey(Customers, on_delete=models.CASCADE)
+    address = models.TextField()
+    dateOfSales = models.DateField()
+
+
+class WishList(Model):
+    customer = models.ForeignKey(Customers, on_delete=models.CASCADE)
+
+
+class WL_P(Model2):
+    product = models.ForeignKey(Products, on_delete=models.CASCADE)
+    wishList = models.ForeignKey(WishList, on_delete=models.CASCADE)
+    
+    
--- main.py
+++ main.py
@@ -1,4 +1,17 @@
 from db.models import Product
 
-product = Product(title='Product1', price=100)
-product.save()
+# product = Product(title='Product1', price=100)
+# product.save()
+
+
+# product = Product.objects.get(id=1)
+
+# product = Product.objects.all()
+
+
+# product = Product.objects.get(id=1)
+# product.title = 'Product updated'
+# product.save()
+
+# product = Product.objects.get(id=2)
+# product.delete()
